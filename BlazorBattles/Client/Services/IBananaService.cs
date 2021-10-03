0316db712272b134ae619f21ff82354b01ae517f
Raphael Rockus <raphaelrockus@gmail.com>
2021-10-03 17:22:31 +0200

    Create Unit Class

---
+++ BlazorBattles/Client/Services/IBananaService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    interface IBananaService
+    {
+        event Action OnChange;
+
+        int Bananas { get; set; }
+
+        void EatBananas(int amount);
+        void AddBananas(int amount);
+    }
+}
